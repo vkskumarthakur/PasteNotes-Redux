d9c663ba2fb75d9026209a7df617cdf590ae6d91
vikas <vkskumarthakur@gmail.com>
2024-06-28 00:43:22 +0530

    edit icon is now added for the card section

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -43,6 +43,16 @@ const Cards = () => {
                 <p className="text-center w-full">{notes.title}</p>
                 <div
                   className="rounded-full cursor-pointer group transition-all delay-75 p-2 hover:bg-white"
+                  title="Edit Note"
+                  onClick={() => {
+                    handleUpdateTodo(notes.id);
+                  }}
+                >
+                  <svg xmlns="http://www.w3.org/2000/svg" width={18} height={18} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-pencil"><path d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z" /><path d="m15 5 4 4" /></svg>
+
+                </div>
+                <div
+                  className="rounded-full cursor-pointer group transition-all delay-75 p-2 hover:bg-white"
                   title="Delete Note"
                   onClick={() => {
                     handleDeleteTodo(notes.id);
